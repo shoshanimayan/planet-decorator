e31b66f18d38d1cdfbecb56d7c00c009d31455a3
Mayan Shoshani <shoshanimayan@gmail.com>
2026-05-17 13:25:55 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    build: { outDir: '..' },
   // If your repo is https://github.com, set base to '/my-repo/'
   base: '/planet-decorator/', 
 })
